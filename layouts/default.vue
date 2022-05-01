c794be65b821d7846e33a504141eea6949b7aa85
alireza mohammadi <a.rezamohmmadi369@gmail.com>
2022-05-01 23:37:19 +0430

    main code

---
+++ layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <Header/>
+        <Nuxt/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BlogDefault',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style  scoped>
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    }
+body{
+    font-family: sans-serif;
+}
+a{
+    text-decoration: none;
+}
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>about page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BlogAbout',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
